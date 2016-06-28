c35f8520c5969088129e5ccab2d865852efd8998
skipperguy12 <cskreem@gmail.com>
2016-06-28 02:08:11 -0400

    Add void tracker

---
+++ src/main/java/pw/kmp/tracker/trackers/space/VoidCause.java
@@ -0,0 +1,10 @@
+package pw.kmp.tracker.trackers.space;
+
+import pw.kmp.tracker.damage.DamageCause;
+
+public class VoidCause extends DamageCause {
+    @Override
+    public String getName() {
+        return "Void";
+    }
+}
