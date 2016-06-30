09054229194cef305627d25056b37dbbf5add731
skipperguy12 <cskreem@gmail.com>
2016-06-29 21:29:22 -0400

    Format messages, rework default messages to fall through, disable fall tracker messages and fallback to default

--- src/main/java/pw/kmp/tracker/listeners/DeathListener.java
+++ src/main/java/pw/kmp/tracker/listeners/DeathListener.java
@@ -1,5 +1,7 @@
 package pw.kmp.tracker.listeners;
 
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -34,8 +36,11 @@ public class DeathListener implements Listener {
 
     }
 
-    private void setMessage(PlayerDeathEvent event, String newMessage) {
-        event.setDeathMessage(newMessage);
+    private void setMessage(PlayerDeathEvent event, String newMessage, Object... names) {
+        for(int i = 0 ; i < names.length ; i++)
+            names[i] = ChatColor.RED.toString() + names[i] + ChatColor.GRAY;
+
+        event.setDeathMessage( String.format(ChatColor.GRAY + newMessage, names));
     }
 
     @EventHandler
@@ -47,67 +52,54 @@ public class DeathListener implements Listener {
 
         // Commented out means that our tracker would add either the same amount or less info than MC's default messages
         switch(causeSwitch) {
-            case AnvilCause:
-                //setMessage(event, event.getEntity().getDisplayName() + " was squashed by a falling anvil.");
-                break;
             case EntityAnvilCause:
-                setMessage(event, event.getEntity().getDisplayName() + " was squashed by " + ((AnvilCause.EntityAnvilCause) cause).getEntity().getName() + "'s falling anvil");
+                setMessage(event,  "%s was squashed by %s falling anvil", event.getEntity().getDisplayName(), ((AnvilCause.EntityAnvilCause) cause).getEntity().getName() + "'s");
                 if(event.getEntity().equals(((AnvilCause.EntityAnvilCause) cause).getEntity()))
-                    setMessage(event, event.getEntity().getDisplayName() + " was squashed by their own falling anvil");
-                break;
-            case BlockCause:
-                //setMessage(event, event.getEntity().getDisplayName() + " was killed by a " + ((BlockCause) cause).getDamager().getBlock().getType().toString().toLowerCase());
+                    setMessage(event,  "%s was squashed by their own falling anvil", event.getEntity().getDisplayName());
                 break;
             case DispensedProjectileCause:
                 if(((DispensedProjectileCause) cause).getEntity() != null)
-                    setMessage(event, event.getEntity().getDisplayName() + " was shot by " + ((DispensedProjectileCause) cause).getEntity().getName() + "'s dispenser"  + " (" + ((int) Math.ceil(((DispensedProjectileCause) cause).getDistance())) + ")");
+                    setMessage(event,  "%s was shot by %s dispenser"  + "(%s)", event.getEntity().getDisplayName(), ((DispensedProjectileCause) cause).getEntity().getName() + "'s", ChatColor.GRAY.toString() + ((DispensedProjectileCause) cause).getDistance().intValue());
                 else
-                    setMessage(event, event.getEntity().getDisplayName() + " was shot by a dispenser " + " (" + ((int) Math.ceil(((DispensedProjectileCause) cause).getDistance())) + ")");
-                break;
-            case ExplosionCause:
-                //setMessage(event, event.getEntity().getDisplayName() + " was blown up.");
+                    setMessage(event,  "%s was shot by a dispenser " + "(%s)", event.getEntity().getDisplayName(), ChatColor.GRAY.toString() + ((DispensedProjectileCause) cause).getDistance().intValue());
                 break;
             case EntityExplosionCause:
-                setMessage(event, event.getEntity().getDisplayName() + " was blown up by " + ((ExplosionCause.EntityExplosionCause) cause).getEntity().getName());
-                break;
-            case FallCause:
-                //setMessage(event, event.getEntity().getDisplayName() + " fell from a height to their death.");
-                break;
-            case EntityFallCause:
-                if (((FallCause.EntityFallCause) cause).getEntity() instanceof Player) {
-                    setMessage(event, event.getEntity().getDisplayName() + " fell at the hands of " + ((FallCause.EntityFallCause) cause).getEntity().getName() );
-                } else {
-                    setMessage(event, event.getEntity().getDisplayName() + " fell at the hands of a " + ((FallCause.EntityFallCause) cause).getEntity().getName());
-                }
-                break;
-            case LavaDamageCause:
-                //setMessage(event, event.getEntity().getDisplayName() + " burned to death in lava.");
-                break;
-            case LightningCause:
-                //setMessage(event, event.getEntity().getDisplayName() + " was struck by lightning and electrocuted to death.");
+                setMessage(event,  "%s was blown up by %s", event.getEntity().getDisplayName(), ((ExplosionCause.EntityExplosionCause) cause).getEntity().getName());
                 break;
             case MeleeCause:
-                setMessage(event, event.getEntity().getDisplayName() + " was slain by " + ((MeleeCause) cause).getEntity().getName() + "'s " + ((MeleeCause) cause).getWeapon().getType().toString().toLowerCase().replace("_", " "));
-                break;
-            case MobAttackCause:
-                // MC's mob tracking is pretty good :)
-                //setMessage(event, event.getEntity().getDisplayName() + " was slain by " + ((MeleeCause) cause).getEntity().getName() + "'s " + ((MeleeCause) cause).getWeapon().getType().toString().toLowerCase().replace("_", ""));
-                break;
-            case PotionDamageCause:
+                setMessage(event,  "%s was slain by %s %s", event.getEntity().getDisplayName(), ((MeleeCause) cause).getEntity().getName() + "'s", ChatColor.GRAY + ((MeleeCause) cause).getWeapon().getType().toString().toLowerCase().replace("_", " "));
                 break;
             case EntityPotionDamageCause:
-                setMessage(event, event.getEntity().getDisplayName() + " was killed with " + ((PotionDamageCause.EntityPotionDamageCause) cause).getEntity().getName() + "'s witchcraft");
+                setMessage(event,  "%s was killed with %s witchcraft", event.getEntity().getDisplayName(), ((PotionDamageCause.EntityPotionDamageCause) cause).getEntity().getName() + "'s");
                 break;
             case ProjectileCause:
-                setMessage(event, event.getEntity().getDisplayName() + " shot by " + ((ProjectileCause) cause).getEntity().getName() + " (" + ((int) Math.ceil(((ProjectileCause) cause).getDistance())) + ")");
+                setMessage(event, "%s was shot by %s (%s)", event.getEntity().getDisplayName(), ((ProjectileCause) cause).getEntity().getName(),  ChatColor.GRAY.toString() +((DispensedProjectileCause) cause).getDistance().intValue());
                 break;
+
+            case EntityFallCause: // TODO: Get this working as @kblks expects it to work. Faults: Falling credit is not timed, and burning -> triggers fall cause
+                //setMessage(event,  "%s fell at the hands of " + (((FallCause.EntityFallCause) cause).getEntity() instanceof Player ? "" : "a ") + "%s", event.getEntity().getDisplayName(), ((FallCause.EntityFallCause) cause).getEntity().getName());
+                //break;
+            case ExplosionCause:
+                //setMessage(event,  "%s was blown up.");
+            case AnvilCause:
+                //setMessage(event,  "%s was squashed by a falling anvil.");
+            case BlockCause:
+                //setMessage(event,  "%s was killed by a " + ((BlockCause) cause).getDamager().getBlock().getType().toString().toLowerCase());
+            case FallCause:
+                //setMessage(event,  "%s fell from a height to their death.");
+            case MobAttackCause:
+                // MC's mob tracking is pretty good :)
+                //setMessage(event,  "%s was slain by " + ((MeleeCause) cause).getEntity().getName() + "'s " + ((MeleeCause) cause).getWeapon().getType().toString().toLowerCase().replace("_", ""));
+            case LavaDamageCause:
+                //setMessage(event,  "%s burned to death in lava.");
+            case LightningCause:
+                //setMessage(event,  "%s was struck by lightning and electrocuted to death.");
+            case PotionDamageCause:
             case VoidCause:
-                break;
             case SuffocationCause:
-                break;
             case WaterCause:
-                break;
             case UnknownCause:
+                event.setDeathMessage(event.getDeathMessage().replace(event.getEntity().getDisplayName(), ChatColor.RED + event.getEntity().getDisplayName() + ChatColor.GRAY));
                 break;
         }
     }
